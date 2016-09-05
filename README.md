ee03e8e0932c5b97df419da195e687d06649214f
Esteban Herrera <estebanhb2@yahoo.com.mx>
2016-09-04 23:24:46 -0500

    first commit

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
-# Getting Started with Slackbots
+# Slackbots and natural language
 
-This Slack bot is a wrapper for the Numbers API that shows how to receive and send simple messages and initiate a conversation using BotKit.
+This Slackbot is a wrapper for the Numbers API that shows how to process natural language using Wit.ai and BotKit.
 
 You can follow the [tutorial](http://sitepoint.com) to build this application or jump straight to the code.
 
 # Requirements
 - A Slack account with administrative privileges
+- A Wit.ai account
 - [Node.js](https://nodejs.org/en/download/)
 
 # Installation
 1. Clone this branch and `cd` into it.
-2. Go to https://my.slack.com/apps/build/custom-integration and create a Bot
-3. Copy the token given by Slack
-4. Start the bot with `token=<YOUR_BOT_TOKEN> node bot.js`
-5. Invite the bot to one of your team's channels with `/invite @<NAME_OF_YOUR_BOT>`
-6. Play with the bot. It will give a random fact every time a number is mentioned in a message. Or initiate a conversation with either a mention, a direct mention, or a direct message with the word `trivia`, for example, `@<NAME_OF_YOUR_BOT> trivia`.
+2. Go to https://my.slack.com/apps/build/custom-integration and create a Bot.
+3. Copy the token given by Slack.
+4. Go to Wit.ai and create a new app by importing `NumberBotApp.zip`.
+5. Once the app is created and the circle next to its name is green, go to the Settings tab and copy your Server Access Token (if none is shown, create one by clicking the *Reset Token* icon).
+4. Start the bot with `token=<YOUR_BOT_TOKEN> wit_token=<YOUR_WIT_AI_SERVER_ACCESS_TOKEN> node bot.js`.
+5. Invite the bot to one of your team's channels with `/invite @<NAME_OF_YOUR_BOT>`.
+6. Play with the bot. It will give a random fact every time a number is mentioned in a message. Or ask for a general, math, or date fact, with, for example:
+    - Give me some general fact about one
+    - What's with 10 as a date?
+    - What can you tell me about twenty?
+    - Tell me something interesting about 7
+    - Give me a date fact about 4
+    - Give me a math fact
+7. If the bot doesn't recognize one of your messages, or you want it to recognize more phrases, you can train it on Wit.ai in the *Understanding* tab or by validating the messages on the *Inbox* tab.
 
 # License
 MIT
--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 const Botkit = require('botkit');
 const request = require('superagent');
+const Wit = require('node-wit').Wit;
+const log = require('node-wit').log;
 
 // Cheking for the token
 if (!process.env.token) {
-    console.log('Error: Specify a token in an environment variable');
+    console.log('Error: Specify a Slack token in an environment variable');
+    process.exit(1);
+}
+
+if (!process.env.wit_token) {
+    console.log('Error: Specify a Wit token in an environment variable');
     process.exit(1);
 }
 
@@ -36,83 +43,136 @@ controller.hears(['[0-9]+'], ['ambient'], (bot, message) => {
     );
 });
 
-// When someone sends a message to the bot with the word trivia
-controller.hears(['trivia'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-    bot.startConversation(message, (err, convo) => {
+// Wit.ai bot specific code
 
-        const askParameter = (response, convoAskParameter, text) => { 
-            convoAskParameter.ask(text, (response, convoAsk) => {
-                convoAsk.say('All right, let me see...');
-                convoAsk.next(); // End of the conversation
-            }, { key: 'number' });
-        };
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {userId: userId, context: sessionState, bot: botObject, message: messageObject}
+const sessions = {};
 
-        convo.ask('What kind of trivia do you want? GENERAL, MATH, or DATE',
-            [
-                // When the answer is general
-                {
-                    pattern: 'general',
-                    callback: (response, convoCallback) => {
-                        askParameter(response, convoCallback, 'Great, give me either a number or the keyword random');
-                        convoCallback.next();
-                    }
-                },
-                // When the answer is math
-                {
-                    pattern: 'math',
-                    callback: (response, convoCallback) => {
-                        askParameter(response, convoCallback, 'Great, give me either a number or the keyword random');
-                        convoCallback.next();
-                    }
-                },
-                // When the answer is date
-                {
-                    pattern: 'date',
-                    callback: (response, convoCallback) => {
-                        askParameter(response, convoCallback, 'Great, give me either a number, or a day of year in the form month/day (eg. 5/15), or the keyword random');
-                        convoCallback.next();
-                    }
-                },
-                // When no valid answer is provided
-                {
-                    default: true,
-                    callback: (response, convoCallback) => {
-                        convoCallback.repeat(); // just repeat the question
-                        convoCallback.next();
-                    }
-                }
-            ], { key: 'type' }
-        );
-
-        // When the conversations ends
-        convo.on('end', convoEnd => {
-            // And the flow is completed
-            if (convoEnd.status=='completed') {
-                // Extract all the responses in the form of a dictionary
-                //let responses = convoEnd.extractResponses();
-
-                // Extract the specific responses by key
-                const type  = convoEnd.extractResponse('type').toLowerCase() !== 'general' 
-                                    ? convoEnd.extractResponse('type').toLowerCase() 
-                                    : '';
-                const number  = convoEnd.extractResponse('number').toLowerCase();
-                
-                // Make the request to the API
-                request
-                    .get(`http://numbersapi.com/${number}/${type}`)
-                    .end((err, res) => {
-                        if(err) {
-                            bot.reply(message, 'Sorry, I couldn\'t process your request');
-                        } else {
-                            bot.reply(message, res.text);
-                        }
-                    }
-                );
-            } else {
-                bot.reply('Sorry, something happened that caused the conversation to stop prematurely');
-            }
-
-        });
+const findOrCreateSession = (userId, bot, message) => {
+    let sessionId = null;
+    // Let's see if we already have a session for the user
+    Object.keys(sessions).forEach(key => {
+        if (sessions[key].userId === userId) {
+            // Yep, got it!
+            sessionId = key;
+        }
     });
+    if (!sessionId) {
+        // No session found for user, let's create a new one
+        sessionId = new Date().toISOString();
+        sessions[sessionId] = {userId: userId, context: {}, bot: bot, message: message};
+    }
+    return sessionId;
+};
 
+//Extract an entity value from the entities returned by Wit
+const firstEntityValue = (entities, entity) => {
+    const val = entities && entities[entity] &&
+        Array.isArray(entities[entity]) &&
+        entities[entity].length > 0 &&
+        entities[entity][0].value;
+    
+    if (!val) {
+        return null;
+    }
+    return typeof val === 'object' ? val.value : val;
+};
+
+// Our bot actions
+const actions = {
+    send(req, res) {
+        const sessionId = req.sessionId;
+        const text = res.text;
+        // Our bot has something to say!
+        // Let's retrieve the bot and message objects to post a message
+        const bot = sessions[sessionId].bot;
+        const message = sessions[sessionId].message;
+    
+        // We return a promise to let our bot know when we're done sending
+        return new Promise(function(resolve) {
+            bot.reply(message, text);
+            return resolve();
+        });
+    },
+    getTrivia(req) {
+        return new Promise(function(resolve) {
+            const context = req.context; 
+            const entities = req.entities;
+
+            const intent = firstEntityValue(entities, 'intent');
+            const rawType = firstEntityValue(entities, 'type');
+            const type = (rawType
+                        ? rawType !== 'general' ? rawType : ''
+                        : context.type)
+                     || '';
+            const random = firstEntityValue(entities, 'random');
+            const number = random ? 'random' : firstEntityValue(entities, 'number');
+        
+            if((intent && intent === 'trivia') || number) {
+                if(number) {
+                    // Make the request to the API
+                    request
+                        .get(`http://numbersapi.com/${number}/${type}`)
+                        .end((err, res) => {
+                            if(err) {
+                                context.response =  'Sorry, I couldn\'t process your request';
+                            } else {
+                                context.response = res.text;
+                            }
+                            context.done = true;
+                            delete context.missingNumber;
+                            return resolve(context);
+                        }
+                    ); 
+                }  else {
+                    context.type = type;
+                    context.missingNumber = true;
+                    return resolve(context);
+                }         
+            } else {
+                context.response = 'Sorry, I didn\'t understand what you want. I\'m still just a bot, can you try again?';
+                context.done = true;
+                return resolve(context);
+            }
+        });
+    },
+};
+
+// Setting up our bot
+const wit = new Wit({
+    accessToken: process.env.wit_token,
+    actions,
+    logger: new log.Logger(log.INFO)
+});
+
+controller.hears(['(.*)'], ['direct_mention', 'mention'], (bot, message) => {
+    const text = message.match[0];
+
+    // We retrieve the user's current session, or create one if it doesn't exist
+    // This is needed for our bot to figure out the conversation history
+    const sessionId = findOrCreateSession(message.user, bot, message);
+
+    // Let's forward the message to the Wit.ai Bot Engine
+    // This will run all actions until our bot has nothing left to do
+    wit.runActions(
+        sessionId, // the user's current session
+        text, // the user's message
+        sessions[sessionId].context // the user's current session state
+    ).then((context) => {
+        // Our bot did everything it has to do.
+        // Now it will be waiting for further user messages to proceed.
+
+        // Based on the session state, you might want to reset or update the session.
+        if (context.done) {
+            delete sessions[sessionId];
+        } else {
+            // Updating the user's current session state
+            sessions[sessionId].context = context;
+        }
+    })
+    .catch((err) => {
+        console.error('Got an error from Wit: ', err.stack || err);
+    });
 });
