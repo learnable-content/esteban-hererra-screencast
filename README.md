6800c30ca2295407daef4e5c43af9014b8ecbac5
Esteban Herrera <estebanhb2@yahoo.com.mx>
2016-11-27 00:40:01 -0600

    first commit

--- README.md
+++ README.md
@@ -1,28 +1,40 @@
-# Getting Started With Skype Bots
+# Natural Language Processing with LUIS.ai
 
-This app shows how you can create a simple Skype bot using the Node.js SDK for the Microsoft Bot Framework.
+This app shows how you a Skype bot can process natural language using a LUIS.ai model.
 
 You can follow the [tutorial](http://sitepoint.com) to build this application or jump straight to the code.
 
 # Requirements
-- A Skype account.
-- A Microsoft Account (live, outlook, msn, hotmail, etc).
+- A Microsoft Account.
 - A [Movie DB](https://www.themoviedb.org) account with a free developer API key
-- [Ngrok](https://ngrok.com/).
 - [Node.js](https://nodejs.org/en/download/) version 5 or higher.
+- A [LUIS.ai](https://www.luis.ai) account.
+- To test the bot, either a Skype account and [Ngrok](https://ngrok.com/) or the Microsoft Bot Framework Emulator.
 
 # Installation
 1. Fork this repository into your own Github account.
-2. Start Ngrok in a console window using `ngrok http 3978` and copy the HTTPS URL you're given.
-3. Go to https://dev.botframework.com/ and register a new bot with the following information:
+2. Login to [LUIS.ai](https://www.luis.ai) and import the file `MovieBotLuisApp.json` to create a new app.
+3. Once the app is created, click on *Train* and then *Publish*.
+4. Copy the App Id and the Subscription Key (shown in the example URL when the app is published).
+5. Start Ngrok in a console window using `ngrok http 3978` and copy the HTTPS URL you're given.
+6. Go to https://dev.botframework.com/ and register a new bot with the following information:
    - Name: MovieBot (any name would do)
    - Bot handle: Any available handle (cannot be changed later)
    - Description: A bot that gives movies information (any description would do)
    - Messaging endpoint: https://[NGROK_URL]/api/messages (for example, https://e2d58786.ngrok.io/api/messages)
-4. Copy the MICROSOFT_APP_ID and MICROSOFT_APP_PASSWORD keys (your password is only shown one time).
-5. Start the app with something like MOVIE_DB_API_KEY=xxxxxx MICROSOFT_APP_ID=xxxxxx MICROSOFT_APP_PASSWORD=xxxxxx node bot.js.
-6. Log in to Skype and add the bot from your bot's dashboard with the *Add to Skype button*. 
-7. Play with the bot in Skype by saying `movie`.
+7. Copy the MICROSOFT_APP_ID and MICROSOFT_APP_PASSWORD keys (your password is only shown one time).
+8. Start the app with something like MOVIE_DB_API_KEY=xxxxxx MICROSOFT_APP_ID=xxxxxx MICROSOFT_APP_PASSWORD=xxxxxx LUIS_APP_ID=xxxx LUIS_API_KEY=xxxx node bot.js.
+9. To test the  bot, log in to Skype and add the bot from your bot's dashboard with the *Add to Skype button* or start the Microsoft Bot Framework Emulator and enter your Microsoft App Id and password. 
+10. Play with the bot by asking for movie recommendations with messages like:
+    - Give me a comedy movie
+    - Give me a documentary from 2001
+    - What's the most popular movie from 2010?
+    - What are the best 3 documentaries from 2010?
+    - Can you suggest 5 actions movies?
+11. The bot should recognize the genre, the year, the number of movies to show, and the sort criteria (by votes or popularity). If a message is not recognized correctly, you can train the LUIS model with new utterances. 
 
 # License
 MIT
+
+![Powered by https://www.themoviedb.org](https://www.themoviedb.org/assets/9b3f9c24d9fd5f297ae433eb33d93514/images/v4/logos/408x161-powered-by-rectangle-green.png)
+
--- bot.js
+++ bot.js
@@ -1,22 +1,27 @@
 const builder = require('botbuilder');
 const restify = require('restify');
 const mdb = require('moviedb')(process.env.MOVIE_DB_API_KEY);
+require('datejs');
 
 // Setup Restify Server
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat bot
 const connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD,
 });
 const bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-const intents = new builder.IntentDialog();
+const luisUrl = 'api.projectoxford.ai/luis/v1';
+const luisModelUrl = `https://${luisUrl}/application?id=${process.env.LUIS_APP_ID}&subscription-key=${process.env.LUIS_API_KEY}`;
+
+const recognizer = new builder.LuisRecognizer(luisModelUrl);
+const intents = new builder.IntentDialog({ recognizers: [recognizer] });
 bot.dialog('/', intents);
 
 // Get this information with mdb.configuration()
@@ -58,88 +63,148 @@ const genres = {
 };
 
 const getMovie = (session) => {
+  // Send typing message
   session.sendTyping();
 
-  mdb.discoverMovie({
-    sort_by: 'popularity.desc',
-    with_genres: session.dialogData.genre,
-    primary_release_year: session.dialogData.year,
-  }, (err, res) => {
-    const msg = new builder.Message(session);
+  const params = {};
+
+  if (session.dialogData.genre) {
+    params.with_genres = session.dialogData.genre;
+  }
+  if (session.dialogData.sort) {
+    params.sort_by = session.dialogData.sort === 'popularity' ? 'popularity.desc' : 'vote_average.desc';
+  }
+  if (session.dialogData.year) {
+    params.primary_release_year = session.dialogData.year;
+  }
+
+  // Call the Movie DB API passing the genre and release year and sorting by popularity
+  mdb.discoverMovie(params, (err, res) => {
+    let msg = new builder.Message(session);
+
+    // If there's no error
     if (!err) {
       const movies = res.results;
-      const index = Math.floor(Math.random() * movies.length);
+      let number = session.dialogData.number ? session.dialogData.number : 1;
+      const maxMoviesToShow = 20;
 
-      msg.text('I found this movie: \n\n**%(title)s**\n\n*%(overview)s*', movies[index]);
-
-      if (movies[index].poster_path) {
-        msg.attachments([{
-          contentType: 'image/jpeg',
-          contentUrl: imagesBaseUrl + posterSize + movies[index].poster_path,
-        }]);
+      if (number > maxMoviesToShow) {
+        number = maxMoviesToShow;
+        session.send(`Sorry, I can only show the first ${maxMoviesToShow} movies:\n\n`);
       }
-    } else {
-      msg.text('Oops, an error, can you please say \'movie\' again?');
+
+      movies.slice(0, number).forEach((movie) => {
+        msg.text('**%(title)s**\n\n*%(overview)s*', movie);
+
+        // If the movie has a poster image
+        if (movie.poster_path) {
+          // Add the image as a message attachment
+          msg.attachments([{
+            contentType: 'image/jpeg',
+            contentUrl: imagesBaseUrl + posterSize + movie.poster_path,
+          }]);
+        }
+        session.send(msg);
+        msg = new builder.Message(session);
+      });
+    } else { // There's an error
+      msg.text('Oops, an error, can you please try again?');
+      session.send(msg);
     }
 
-    session.endDialog(msg);
+    // End the dialog
+    session.endDialog();
   });
 };
 
-intents.onDefault([
+// If there's no match
+intents.matches('Hello', [
   (session, args, next) => {
     if (!session.userData.name) {
+      // Begin the askName dialog
       session.beginDialog('/askName');
     } else {
+      // Execute the next function
       next();
     }
   },
   session =>
-    session.send(`I'm new around here %s. I only know the 'movie' command, say it if you want a movie recommendation`, session.userData.name),
+    session.send('Hi %s. I know about movies, ask me for recommendations...', session.userData.name),
+])
+
+// When LUIS.ai couldn't match a better intent
+.matches('None', (session) => {
+  session.send(`Sorry I didn't understand, I'm a bot that only knows about movies`);
+})
+
+// When a user message matches the intent 'movie'
+.matches('Movie', [
+  (session, args, next) => {
+    // Resolve and store any entities passed from LUIS.
+    const genreEntity = builder.EntityRecognizer.findEntity(args.entities, 'genre');
+    const sortPopularityEntity = builder.EntityRecognizer.findEntity(args.entities, 'sort::popularity');
+    const numberEntity = builder.EntityRecognizer.findEntity(args.entities, 'number');
+    const dateEntity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.datetime.date');
+
+    // If there's a genre, find the best match to extract the correct genre object
+    if (genreEntity) {
+      const match = builder.EntityRecognizer.findBestMatch(genres, genreEntity.entity);
+      const genreObj = match ? genres[match.entity] : null;
+      session.dialogData.genre = genreObj ? genreObj.id : null;
+    }
+
+    // If there's a date, parse it to get the year
+    if (dateEntity) {
+      const date = Date.parse(dateEntity.resolution.date);
+      session.dialogData.year = date ? date.getFullYear() : null;
+    }
+
+    // If there's a number, that number should be the year
+    if (numberEntity) {
+      session.dialogData.number = (dateEntity && dateEntity.resolution.date === numberEntity.entity)
+                                    ? 1
+                                    : numberEntity.entity;
+    }
+
+    session.dialogData.sort = sortPopularityEntity ? 'popularity' : 'votes';
+
+    next();
+  },
+  // Then ask for the release year if none is recognized
+  (session, results, next) => {
+    if (!session.dialogData.year) {
+      session.beginDialog('/yearPrompt');
+    } else {
+      next();
+    }
+  },
+  // Finally call the Movie DB API
+  (session, results) => {
+    if (results && results.response) {
+      session.dialogData.year = results.response;
+    }
+    getMovie(session);
+  },
 ]);
 
+// askName dialog
 bot.dialog('/askName', [
   session =>
     builder.Prompts.text(session, `Hi! I'm MovieBot. What's your name?`),
   (session, results) => {
-    // should we be mutating the session like this?
+    // Store the user's name on the userData session attribute
     session.userData.name = results.response;
     session.endDialog('Hello %s', session.userData.name);
   },
 ]);
 
-intents.matches(/^movie/i, [
-  session =>
-    session.beginDialog('/genrePrompt'),
-  (session, results) => {
-    if (results.response.id > 0) {
-      session.dialogData.genre = results.response.id;
-      session.beginDialog('/yearPrompt');
-    } else {
-      session.send('Okay, maybe next time');
-      session.endDialog();
-    }
-  },
-  (session, results) => {
-    session.dialogData.year = results.response;
-    getMovie(session);
-  },
-]);
-
-bot.dialog('/genrePrompt', [
-  session =>
-    builder.Prompts.choice(session, 'What genre would you like?', genres),
-  (session, results) => {
-    const choice = genres[results.response.entity.toLowerCase()];
-
-    session.endDialogWithResult({ response: choice });
-  },
-]);
-
+// Year Prompt Dialog
 bot.dialog('/yearPrompt', [
   session =>
-    builder.Prompts.text(session, `Enter a release year (in the format yyyy) if you want to specify one or just respond with something like 'no' otherwise`),
+    builder.Prompts.text(session,
+      `Enter a release year (in the format yyyy) if you want to specify one or just respond with something like 'no' otherwise`),
   (session, results) => {
+    // there's a match for something that seems like a year?
     const matched = results.response.match(/\d{4}/g);
 
     session.endDialogWithResult({ response: matched });
